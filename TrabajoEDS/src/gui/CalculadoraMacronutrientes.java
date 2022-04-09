876c36fad8b7c45e190dac73e5d27d684e36eea8
AluInia <maregipon@alu.edu.gva.es>
2022-04-09 12:19:15 +0200

    Ahora el panel con la lista de los alimentos tiene un encabezado y mostrará los valores del alimento.
    Los alimentos creados por defecto no se podrán eliminar.
    
    Eliminados imports inutilizados.
    
    Refactorizacion del cálculo del desglose de macronutrientes del usuario.

--- TrabajoEDS/src/gui/CalculadoraMacronutrientes.java
+++ TrabajoEDS/src/gui/CalculadoraMacronutrientes.java
@@ -7,6 +7,8 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -15,6 +17,7 @@ import javax.swing.JTextPane;
 import javax.swing.JComboBox;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 
 import calculable.Calculadora;
 import calculable.CalculadoraHombre;
@@ -29,11 +32,12 @@ import java.awt.event.ItemListener;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.GridLayout;
 
 import trabajoEDS.*;
-import calculable.*;
 
 public class CalculadoraMacronutrientes {
 
@@ -42,7 +46,7 @@ public class CalculadoraMacronutrientes {
 	private JFrame JFrame;	
 	private JPanel panelListaAlimentos;
 	JScrollPane scroll;	
-	
+
 	// -- Fields Persona -- //
 
 	private JTextField textFieldNombrePers;
@@ -52,7 +56,7 @@ public class CalculadoraMacronutrientes {
 	private JTextField textFieldAltura;
 	private JComboBox<ActividadFisica> comboBoxActFisica;
 	private JComboBox<Genero> comboBoxSexo;
-	
+
 	// Fields Alimento //
 
 	private JTextField textFieldGrasas;
@@ -66,14 +70,14 @@ public class CalculadoraMacronutrientes {
 	private JTextField textFieldVitaminas;
 	private JTextField textFieldHierro;
 	private JTextField textFieldEnergia;
-	
+
 	// Fields Calculo //
 
 	private JTextField textFieldGrasasTot;
 	private JTextField textFieldAzucarTot;
 	private JTextField textFieldSalTot;
 	private JTextField textFieldEnergiaTot;
-	
+
 	private Persona usuario;
 	private ArrayList<AlimentoCantidad> alimentosCantidad = new ArrayList<AlimentoCantidad>();
 	private ArrayList<Alimento> alimentos = new ArrayList<Alimento>();
@@ -104,18 +108,18 @@ public class CalculadoraMacronutrientes {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() {		
 		JFrame = new JFrame();
 		JFrame.setTitle("Calculadora de macronutrientes");
 		JFrame.setBounds(100, 100, 1298, 773);
-		JFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		JFrame.getContentPane().setLayout(null);
 
 		panelListaAlimentos = new JPanel();		
 		scroll = new JScrollPane(panelListaAlimentos);
 		panelListaAlimentos.setBackground(new Color(250, 209, 255));
 		panelListaAlimentos.setBackground(new Color(250, 209, 255));
-		panelListaAlimentos.setLayout(new GridLayout(0, 3, 0, 0));
+		panelListaAlimentos.setLayout(new GridLayout(0, 12, 0, 5));
 		scroll.setBounds(37, 174, 960, 333);		
 		JFrame.getContentPane().add(scroll);
 
@@ -282,7 +286,7 @@ public class CalculadoraMacronutrientes {
 			/**
 			 * Comprueba que los TextField del panel persona estan todos rellenos sino muestra una ventana de dialogo para informar al usuario
 			 * Inicializa el usuario con los datos en el panel persona
-			 * Calcula el desglose de los macronutrientes adecuado para la persona creada
+			 * Calcula el desglose de los macronutrientes para la persona creada
 			 * Pinta el desglose en los campos correspondientes de la GUI
 			 * 
 			 * @author Marta
@@ -295,17 +299,8 @@ public class CalculadoraMacronutrientes {
 				}
 				else {
 					usuario = crearPersona();				
-					Calculadora calculadora;
 
-					if(usuario.getGenero().equals(Genero.FEMENINO)) {
-						calculadora = new CalculadoraMujer();
-					}
-					else {
-						calculadora = new CalculadoraHombre();
-						System.out.println("Soy hombre");
-					}
-					
-					DistribucionMacronutrientes desgloseMacrosUsuario = calculadora.calculoHabitosCorrectos(usuario);
+					DistribucionMacronutrientes desgloseMacrosUsuario = calculateDesgloseMacrosUsuario();
 					paintDesgloseMacrosUsuario(desgloseMacrosUsuario);
 				}
 			}
@@ -328,11 +323,11 @@ public class CalculadoraMacronutrientes {
 			public void actionPerformed(ActionEvent arg0) {
 				Alimento alimento = crearAlimento();		
 				alimentos.add(alimento);
-				
+
 				addRowAlimento(alimento);
 				scroll.revalidate();
 				scroll.repaint();
-				limpiarPanelAlimento();
+				//limpiarPanelAlimento();
 			}
 		});
 		btnAddAlimento.setBackground(new Color(191, 234, 237));
@@ -501,8 +496,10 @@ public class CalculadoraMacronutrientes {
 		backgroundListado.setBounds(10, 104, 1015, 628);
 		JFrame.getContentPane().add(backgroundListado);
 
+
+		intializePanelListaAlimentos();
 	}
-	
+
 	/**
 	 * 
 	 * @return
@@ -518,8 +515,10 @@ public class CalculadoraMacronutrientes {
 		String saturadasS = textFieldSaturadas.getText();
 		String proteinasS = textFieldProteinas.getText();
 		String salS = textFieldSal.getText();
-		
-		
+		String hierroS = textFieldHierro.getText();
+		String calcioS = textFieldCalcio.getText();
+
+
 		Double energia = Double.parseDouble(energiaS);
 		Double hidratos = Double.parseDouble(hidratosS);
 		Double azucar = Double.parseDouble(azucarS);
@@ -527,7 +526,9 @@ public class CalculadoraMacronutrientes {
 		Double saturadas = Double.parseDouble(saturadasS);
 		Double proteinas = Double.parseDouble(proteinasS);
 		Double sal = Double.parseDouble(salS);
-		
+		Double hierro = Double.parseDouble(hierroS);
+		Double calcio = Double.parseDouble(calcioS);
+
 		alimento.setNombre(nombre);
 		alimento.setEnergia(energia);
 		alimento.setHidratosCarbono(hidratos);
@@ -535,7 +536,9 @@ public class CalculadoraMacronutrientes {
 		alimento.setGrasas(grasas);
 		alimento.setGrasasSaturadas(saturadas);
 		alimento.setProteinas(proteinas);
-		alimento.setSal(sal);		
+		alimento.setSal(sal);
+		alimento.setHierro(hierro);
+		alimento.setCalcio(calcio);
 
 		return alimento;
 	}
@@ -598,29 +601,102 @@ public class CalculadoraMacronutrientes {
 	}
 
 	/**
+	 * Añade una nueva fila al panelListaAlimentos con la informacion del alimento
+	 * 
 	 * Crea un AlimentoCantidad asignando alimento y 0 a los gramos.
-	 * Crea un TextField para introducir los gramos consumidos y un JCheckBox con el nombre del alimento
+	 * Crea un TextField para introducir los gramos consumidos, un JCheckBox con el nombre del alimento y JLabel con la informacion del alimento
 	 *  
-	 * @param alimento alimento a aÃ±adir
+	 * @param alimento alimento a añadir
 	 * @author Marta
 	 */
 	private void addRowAlimento(Alimento alimento) {
+		AlimentoCantidad alimentoCantidad = new AlimentoCantidad(alimento, 0);
+		alimentosCantidad.add(alimentoCantidad);
+
+		//Conversion atributos alimento a String
+		double energia = alimento.getEnergia();
+		String textEnergia = Double.toString(energia);
+
+		double hidratos = alimento.getHidratosCarbono();
+		String textHidratos = Double.toString(hidratos);
+
+		double azucar = alimento.getAzucar();
+		String textAzucar = Double.toString(azucar);
+
+		double grasas = alimento.getGrasas();
+		String textGrasas = Double.toString(grasas);
+
+		double grasasSaturadas = alimento.getGrasasSaturadas();
+		String textGrasasSaturadas = Double.toString(grasasSaturadas);
+
+		double proteinas = alimento.getProteinas();
+		String textProteinas = Double.toString(proteinas);
+
+		double sal = alimento.getSal();
+		String textSal = Double.toString(sal);
+
+		double calcio = alimento.getCalcio();
+		String textCalcio = Double.toString(calcio);
+
+		double hierro = alimento.getHierro();
+		String textHierro = Double.toString(hierro);
+
+		//Añadir elementos GUI
 		JCheckBox chckbxAlimento = new JCheckBox(alimento.getNombre());
+		chckbxAlimento.setHorizontalAlignment(SwingConstants.CENTER);
+		chckbxAlimento.setBackground(new Color(250,209,255));
 		panelListaAlimentos.add(chckbxAlimento);
-		
+
 		JTextField gramosConsumidos = new JTextField();
 		gramosConsumidos.setText("0");
-		panelListaAlimentos.add(gramosConsumidos);
+		gramosConsumidos.setHorizontalAlignment(SwingConstants.CENTER);		
 		gramosConsumidos.setColumns(10);		
 		gramosConsumidos.setEditable(false);
-		
-		JButton btnBorrar = new JButton("Borrar");
+		gramosConsumidos.setBackground(new Color(250,209,255));
+		gramosConsumidos.setBorder(BorderFactory.createEmptyBorder());
+		panelListaAlimentos.add(gramosConsumidos);
+
+		JLabel lblEnergia = new JLabel(textEnergia);
+		lblEnergia.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblEnergia);
+
+		JLabel lblHidratosCarbono = new JLabel(textHidratos);
+		lblHidratosCarbono.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblHidratosCarbono);
+
+		JLabel lblAzucar = new JLabel(textAzucar);
+		lblAzucar.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblAzucar);
+
+		JLabel lblGrasas = new JLabel(textGrasas);
+		lblGrasas.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblGrasas);
+
+		JLabel lblGrasasSaturadas = new JLabel(textGrasasSaturadas);
+		lblGrasasSaturadas.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblGrasasSaturadas);
+
+		JLabel lblProteinas = new JLabel(textProteinas);
+		lblProteinas.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblProteinas);
+
+		JLabel lblSal = new JLabel(textSal);
+		lblSal.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblSal);
+
+		JLabel lblCalcio = new JLabel(textCalcio);
+		lblCalcio.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblCalcio);
+
+		JLabel lblHierro = new JLabel(textHierro);
+		lblHierro.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblHierro);
+
+		JButton btnBorrar = new JButton("Eliminar");
+		btnBorrar.setHorizontalAlignment(SwingConstants.CENTER);
+		btnBorrar.setBackground(new Color(248, 195, 237));
 		panelListaAlimentos.add(btnBorrar);
-		
-		int gramosConsumidosInt = Integer.parseInt(gramosConsumidos.getText());		
-		AlimentoCantidad alimentoCantidad = new AlimentoCantidad(alimento, gramosConsumidosInt);
-		alimentosCantidad.add(alimentoCantidad);
-		
+
 		gramosConsumidos.addFocusListener(new FocusAdapter() {
 			/**
 			 * Al perder el foco se actualizan los gramos de alimentoCantidad por el valor introducido
@@ -634,7 +710,7 @@ public class CalculadoraMacronutrientes {
 				alimentoCantidad.setGramos(gramosConsumidos);
 			}
 		});		
-		
+
 		/**
 		 * Si el checkbox es true el TextField se habilita sino se deshabilita, se pone el valor del TextField a 0
 		 * y se actualizan los gramos de alimentoCantidad a 0
@@ -644,18 +720,20 @@ public class CalculadoraMacronutrientes {
 		chckbxAlimento.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
 				if(e.getStateChange() == ItemEvent.SELECTED){
-					gramosConsumidos.setEditable(true);					
-                }
-                else if(e.getStateChange() == ItemEvent.DESELECTED){
-                	alimentoCantidad.setGramos(0);
-                	gramosConsumidos.setText("0");
-                	gramosConsumidos.setEditable(false);                	
-                }
+					gramosConsumidos.setEditable(true);
+					gramosConsumidos.setBackground(new Color(254, 247, 255));
+				}
+				else if(e.getStateChange() == ItemEvent.DESELECTED){
+					alimentoCantidad.setGramos(0);
+					gramosConsumidos.setText("0");
+					gramosConsumidos.setEditable(false);
+					gramosConsumidos.setBackground(new Color(250,209,255));
+				}
 			}
 		});
-		
+
 		/**
-		 * Elimina el alimentoCantidad de la lista de alimentosCantidad, los elementos de la GUI que componen la fila y repinta el scroll
+		 * Elimina el alimentoCantidad de la lista alimentosCantidad, el alimento de la lista alimentos, los elementos de la GUI que componen la fila y repinta el scroll
 		 * 
 		 * @author Marta
 		 */
@@ -663,13 +741,23 @@ public class CalculadoraMacronutrientes {
 			public void actionPerformed(ActionEvent e) {
 				panelListaAlimentos.remove(chckbxAlimento);
 				panelListaAlimentos.remove(gramosConsumidos);
+				panelListaAlimentos.remove(lblEnergia);
+				panelListaAlimentos.remove(lblHidratosCarbono);
+				panelListaAlimentos.remove(lblAzucar);
+				panelListaAlimentos.remove(lblGrasas);
+				panelListaAlimentos.remove(lblGrasasSaturadas);
+				panelListaAlimentos.remove(lblProteinas);
+				panelListaAlimentos.remove(lblSal);
+				panelListaAlimentos.remove(lblCalcio);
+				panelListaAlimentos.remove(lblHierro);
 				panelListaAlimentos.remove(btnBorrar);
 				scroll.revalidate();
 				scroll.repaint();
-				alimentosCantidad.remove(alimentoCantidad);				
+				alimentosCantidad.remove(alimentoCantidad);	
+				alimentos.remove(alimento);
 			}
 		});
-	}
+	}	
 
 	/**
 	 * Devuelve true si el string de algun TextField del panel persona esta vacio o solo contiene espacios en blanco, sino false
@@ -682,15 +770,15 @@ public class CalculadoraMacronutrientes {
 		String edad = textFieldEdad.getText();
 		String altura = textFieldAltura.getText();
 		String peso = textFieldPeso.getText();
-		
+
 		boolean nombreNotValid = nombre.isBlank() || nombre.isEmpty();
 		boolean edadNotValid   = edad.isBlank()   || edad.isEmpty();
 		boolean alturaNotValid = altura.isBlank() || altura.isEmpty();
 		boolean pesoNotValid   = peso.isBlank()   || peso.isEmpty();
-		
+
 		return nombreNotValid || edadNotValid || alturaNotValid || pesoNotValid;		
 	}
-	
+
 	/**
 	 * Muestra en la GUI el desglose de macronutrientes del usuario y su IMC. 
 	 * Los valores se mostraran con el formato decimal "#.##"
@@ -701,46 +789,149 @@ public class CalculadoraMacronutrientes {
 	 */
 	private void paintDesgloseMacrosUsuario(DistribucionMacronutrientes desgloseMacrosUsuario) {
 		DecimalFormat dformat = new DecimalFormat("#.##");
-		
+
 		//Energia
 		double energiaTotal = desgloseMacrosUsuario.getEnergiaTotal();
 		String energiaTotalText = dformat.format(energiaTotal);
 		textFieldEnergiaTot.setText(energiaTotalText);
-		
+
 		if(!desgloseMacrosUsuario.isCantidadEnergiaAdecuada()) {
 			textFieldEnergiaTot.setForeground(Color.red);
 		}
-		
+
 		//Azucar
 		double azucarTotal = desgloseMacrosUsuario.getAzucarTotal();
 		String azucarTotalText = dformat.format(azucarTotal);
 		textFieldAzucarTot.setText(azucarTotalText);
-		
+
 		if(!desgloseMacrosUsuario.isCantidadAzucarAdecuado()) {
 			textFieldAzucarTot.setForeground(Color.red);
 		}
-		
+
 		//Grasas
 		double grasasTotales = desgloseMacrosUsuario.getGrasasTotales();
 		String grasasTotalesText = dformat.format(grasasTotales);
 		textFieldGrasasTot.setText(grasasTotalesText);
-		
+
 		if(!desgloseMacrosUsuario.isCantidadGrasasAdecuada()) {
 			textFieldGrasasTot.setForeground(Color.red);
 		}
-		
+
 		//Sal
 		double salTotal = desgloseMacrosUsuario.getSalTotal();
 		String salTotalText = dformat.format(salTotal);
 		textFieldSalTot.setText(salTotalText);
-		
+
 		if(!desgloseMacrosUsuario.isCantidadSalAdecuada()) {
 			textFieldSalTot.setForeground(Color.red);
 		}
-		
+
 		//IMC
 		double imc = usuario.getImc();
 		String imcText = dformat.format(imc);
 		textFieldImc.setText(imcText);
 	}
+
+	/**
+	 * Calcula el total de macronutrientes ingeridos por el usuario teniendo en cuenta si es hombre o mujer
+	 * 
+	 * @return desglose de macronutrientes totales del usuario
+	 * @author Marta
+	 */
+	private DistribucionMacronutrientes calculateDesgloseMacrosUsuario() {					
+		Calculadora calculadora;
+		Genero generoUsuario = usuario.getGenero();
+
+		if(generoUsuario.equals(Genero.FEMENINO)) {
+			calculadora = new CalculadoraMujer();
+		}
+		else {
+			calculadora = new CalculadoraHombre();			
+		}
+
+		DistribucionMacronutrientes desgloseMacrosUsuario = calculadora.calculoHabitosCorrectos(usuario);
+
+		return desgloseMacrosUsuario;
+	}
+
+	/**
+	 * Inicializa el panelListaAlimentos
+	 * @author Marta
+	 */
+	private void intializePanelListaAlimentos() {
+		paintCabeceraPanelListaAlimentos();
+		//Inicializar lista alimentos
+		//Pintar lista alimentos		
+		disableButtonsPanelListaAlimentos(); 
+	}
+
+	/**
+	 * Pinta en la GUI la cabecera de panelListaAlimentos
+	 * @author Marta
+	 */
+	private void paintCabeceraPanelListaAlimentos() {
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblNombre);
+
+		JLabel lblCantidad = new JLabel("Gramos consumidos");
+		lblCantidad.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblCantidad);
+
+		JLabel lblEnergia = new JLabel("Kcal");
+		lblEnergia.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblEnergia);
+
+		JLabel lblHidratosCarbono = new JLabel("Hidratos de Carbono");
+		lblHidratosCarbono.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblHidratosCarbono);
+
+		JLabel lblAzucar = new JLabel("Azucar");
+		lblAzucar.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblAzucar);
+
+		JLabel lblGrasas = new JLabel("Grasas");
+		lblGrasas.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblGrasas);
+
+		JLabel lblGrasasSaturadas = new JLabel("Grasas saturadas");
+		lblGrasasSaturadas.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblGrasasSaturadas);
+
+		JLabel lblProteinas = new JLabel("Proteinas");
+		lblProteinas.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblProteinas);
+
+		JLabel lblSal = new JLabel("Sal");
+		lblSal.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblSal);
+
+		JLabel lblCalcio = new JLabel("Calcio");
+		lblCalcio.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblCalcio);
+
+		JLabel lblHierro = new JLabel("Hierro");
+		lblHierro.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblHierro);
+
+		JLabel lblEliminar = new JLabel("Eliminar");
+		lblEliminar.setHorizontalAlignment(SwingConstants.CENTER);
+		panelListaAlimentos.add(lblEliminar);
+	}
+
+	/**
+	 * Deshabilita todos los botones de panelListaAlimentos creados antes de la llamada a este metodo
+	 * @author Marta
+	 */
+	private void disableButtonsPanelListaAlimentos() {
+		Component[] comp = panelListaAlimentos.getComponents();
+		for(int i = 0; i < comp.length; i++) {
+			if(comp[i] instanceof JButton) {
+				JButton button = (JButton) comp[i];
+				button.setEnabled(false);
+			}
+		}
+	}
 }
+
+
