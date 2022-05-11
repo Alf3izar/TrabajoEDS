9ad12ead485a7452cc1f71a06e649fb7bc32a539
Raúl <raul.fernandez.cabanillas@gmail.com>
2022-05-11 13:40:17 +0200

    Añadido JUnit
    Signed-off-by: Raúl <raul.fernandez.cabanillas@gmail.com>

---
+++ TrabajoEDS/src/calculadoras/CalculadoraNutrientesHombrePrueba.java
@@ -0,0 +1,33 @@
+package calculadoras;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraNutrientesHombrePrueba extends CalculadoraNutrientesHombre {
+
+	private static double cantidadEnergiaTotal;
+	private static double cantidadEnergiaAdecuada;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cantidadEnergiaTotal = 2500;
+		cantidadEnergiaAdecuada = 3000;
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		System.out.println("Pruebas finalizadas");
+	}
+
+	@Test
+	void test() {
+		boolean resultado = comprobarSiCantidadEnergiaEsAdecuada(cantidadEnergiaTotal, cantidadEnergiaAdecuada);
+		assertEquals(true,resultado);
+	}
+
+}
